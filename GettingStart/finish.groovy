a757c2469fba02205fbdc75f1f88d753deee34ab
umairarshad <umair6622@gmail.com>
2019-07-09 13:57:34 +0500

    java to groovy...converted

---
+++ GettingStart/finish.groovy
@@ -0,0 +1,14 @@
+@groovy.transform.ToString()
+ class User {
+
+    Long id;
+    String firstName;
+    String lastName;
+    String email;
+    Date dob;
+
+   void printFullName(){
+        println"FullName: $firstName $lastName"
+    }
+
+}
